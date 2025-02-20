3a979c2dd6c15433096e9c1dae6a02a514e29b8c
John Christian Vicente <johnc.vicente1@gmail.com>
2025-02-20 19:40:41 +0800

    Removed unique

--- exam/query.sql
+++ exam/query.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS bills (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    meter_number INTEGER NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
+    meter_number INTEGER NOT NULL,
     address VARCHAR(510) NOT NULL,
     unit INT NOT NULL,
     total DECIMAL(10, 2) NOT NULL,
